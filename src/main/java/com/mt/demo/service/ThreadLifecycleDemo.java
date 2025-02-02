fb9ea3ab72b203a070bd757eabae818bbea1c852
Saurav Sengupta <saurav.sengupta@oracle.com>
2025-02-02 13:42:09 +0530

    Modified ThreadLifecycleDemo

--- src/main/java/com/mt/demo/service/ThreadLifecycleDemo.java
+++ src/main/java/com/mt/demo/service/ThreadLifecycleDemo.java
@@ -17,14 +17,16 @@ public class ThreadLifecycleDemo extends Thread{
 
 
     public ResponseEntity<Void> demoThreadLifecycle() throws InterruptedException {
-        ThreadLifecycleDemo t = new ThreadLifecycleDemo();
-        System.out.println(Thread.currentThread().getName()+"- state of t1= "+t.getState());
-        t.start();
-        System.out.println(Thread.currentThread().getName()+"- state of t1= "+t.getState());
-        Thread.currentThread().sleep(1000);
-        System.out.println(Thread.currentThread().getName()+"- state of t1= "+t.getState());
-        t.join();
-        System.out.println(Thread.currentThread().getName()+"- state of t1= "+t.getState());
+        ThreadLifecycleDemo t1 = new ThreadLifecycleDemo();
+        System.out.println(Thread.currentThread().getName()+"- state of t1= "+t1.getState());
+        t1.start();
+        System.out.println(Thread.currentThread().getName()+"- state of t1= "+t1.getState());
+        Thread.currentThread().sleep(1000); // sleep()  -> current executing thread will
+        // pause itself and give chance to t1 thread to execute itself.
+        System.out.println(Thread.currentThread().getName()+"- state of t1= "+t1.getState());
+        t1.join(); //join() -> current executing will pause itself and wait for t1 to completely
+        // finish its' execution and then the control will be given back to current executing thread.
+        System.out.println(Thread.currentThread().getName()+"- state of t1= "+t1.getState());
         return ResponseEntity.noContent().build();
     }
 }
